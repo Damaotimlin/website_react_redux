1e427ee3ac45feadf7c2d83fe3f43f0ba0d355e9
DamaoTimLin <damao.republic@gmail.com>
2016-11-04 18:22:20 +0800

    Switch development/productiob in .env

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 // Work for react-hot-loader
 var webpack = require('webpack');
+var dotenv = require('dotenv').config();
 var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var developerConfig = require('./webpack.dev.config');
+var productionConfig = require('./webpack.pro.config');
 
-new WebpackDevServer(webpack(config), {
-	publicPath: config.output.publicPath,
-	hot: true,
-	historyApiFallback: true
-}).listen(8081, '0.0.0.0', function (err, result){
-	if (err){
-		return console.log(err);
-	}
+console.log(`process.env.NODE_ENV = ${process.env.NODE_ENV}`)
 
-	console.log('react-hot-loader Listening at http://localhost:8081/');
-});
+if (process.env.NODE_ENV === 'production') {
+	return new WebpackDevServer(webpack(productionConfig), {
+				publicPath: productionConfig.output.publicPath,
+			}).listen(8080, '0.0.0.0', function (err, res){
+				if (err){
+					return console.log(err);
+				}
+				console.log(`Server ${process.env.NODE_ENV} listening at http://0.0.0.0:8080`)
+			});
+	} else {
+		return new WebpackDevServer(webpack(developerConfig), {
+			publicPath: developerConfig.output.publicPath,
+			hot: true,
+			historyApiFallback: true
+		}).listen(3000, '0.0.0.0', function (err, result){
+			if (err){
+				return console.log(err);
+			}
+			console.log('react-hot-loader Listening at http://0.0.0.0:3000/');
+		});
+}
